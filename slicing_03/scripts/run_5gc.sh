ca1204f003cb40477e226489284e5d0cb06a1046
Niloy Saha <niloysaha.ns@gmail.com>
2021-05-10 06:06:31 +0000

    add 5gc files.

--- slicing_03/scripts/run_5gc.sh
+++ slicing_03/scripts/run_5gc.sh
@@ -3,7 +3,7 @@
 PID_LIST=()
 cd ~/free5gc
 
-CONF_DIR="${HOME}/5gc-config/slicing_02/config"
+CONF_DIR="${HOME}/5gc-config/slicing_03/config"
 export GIN_MODE=release
 
 NF_LIST_1="nrf amf"
@@ -28,18 +28,12 @@ for NF in ${NF_LIST_2}; do
     sleep 0.1
 done
 
-sudo ./bin/n3iwf &
-SUDO_N3IWF_PID=$!
-sleep 1
-N3IWF_PID=$(pgrep -P $SUDO_N3IWF_PID)
-PID_LIST+=($SUDO_N3IWF_PID $N3IWF_PID)
-
 function terminate()
 {
     sudo kill -SIGTERM ${PID_LIST[${#PID_LIST[@]}-2]} ${PID_LIST[${#PID_LIST[@]}-1]}
     sleep 2
     # clear mongodb
-    cd ${HOME}/5gc-config/slicing_02/scripts
+    cd ${HOME}/5gc-config/slicing_03/scripts
     ./clear_nfinfo.sh
 }
 
