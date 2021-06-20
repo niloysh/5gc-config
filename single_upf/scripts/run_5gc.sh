0ece1b7875ec3455f3c1e278648376a2236c69e1
Niloy Saha <niloysaha.ns@gmail.com>
2021-06-20 06:34:52 +0000

    bugfix: dnn in core config

--- single_upf/scripts/run_5gc.sh
+++ single_upf/scripts/run_5gc.sh
@@ -14,12 +14,6 @@ for NF in ${NF_LIST}; do
     sleep 0.1
 done
 
-sudo ./bin/n3iwf &
-SUDO_N3IWF_PID=$!
-sleep 1
-N3IWF_PID=$(pgrep -P $SUDO_N3IWF_PID)
-PID_LIST+=($SUDO_N3IWF_PID $N3IWF_PID)
-
 function terminate()
 {
     sudo kill -SIGTERM ${PID_LIST[${#PID_LIST[@]}-2]} ${PID_LIST[${#PID_LIST[@]}-1]}
